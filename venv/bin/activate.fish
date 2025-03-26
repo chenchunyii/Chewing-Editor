7647cfc02e8a11549d61c0023325269b2703807f
Chunyi <chun.yii.chen@gmail.com>
2025-03-26 19:21:46 +0800

    test

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/chun/文件/chewing_editor/venv'
+set -gx VIRTUAL_ENV '/home/chun/文件/chewing-editor/venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
